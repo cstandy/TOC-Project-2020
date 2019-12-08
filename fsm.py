587cbda45d7d71ada4b1593aa58402f61920d7e5
cstandy <cst.andy@gmail.com>
2019-12-08 20:44:45 +0800

    Feat: Enable to search time zones

--- fsm.py
+++ fsm.py
@@ -2,28 +2,36 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 
+import pytz # python time zone package
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_search(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "search"
 
     def is_going_to_state2(self, event):
         text = event.message.text
         return text.lower() == "go to state2"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    # Auto-binding callback method with 'on_enter_' prefix
+    def on_enter_search(self, event):
+        print("I'm entering search state")
+
+        tz_list = pytz.all_timezones
+        tz_str = ''
+
+        for i in range(len(pytz.all_timezones)):
+            tz_str = tz_str + tz_list[i] + '\n'
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_text_message(reply_token, tz_str)
         self.go_back()
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    def on_exit_search(self):
+        print("Leaving search state")
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
